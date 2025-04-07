3adc00f3b24e797d3aaae98cf0be95d2120a3bef
anushka <81908360+anushka369@users.noreply.github.com>
2025-04-07 22:16:26 +0530

    Update Prisoner Cell View.sql

--- Create Views/Prisoner Cell View.sql
+++ Create Views/Prisoner Cell View.sql
@@ -2,6 +2,11 @@ USE PRISONER_MANAGEMENT;
 
 -- View to display all prisoners with their cell information
 CREATE VIEW PrisonerCellView AS
-SELECT p.prisoner_id, p.first_name, p.last_name, c.cell_number, c.block_number
+SELECT 
+  p.prisoner_id, 
+  p.first_name, 
+  p.last_name, 
+  c.cell_number, 
+  c.block_number
 FROM Prisoner p
 JOIN Cell c ON p.cell_id = c.cell_id;
