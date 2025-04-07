f552e6bb4b922962c802f69774b45df394c57e7b
anushka <81908360+anushka369@users.noreply.github.com>
2025-04-07 22:17:21 +0530

    Update Medical Details View.sql

---
+++ Create Views/Medical Details View.sql
@@ -0,0 +1,13 @@
+USE PRISONER_MANAGEMENT;
+
+-- View to display all medical records with doctor details
+CREATE VIEW MedicalDetailsView AS
+SELECT 
+  mr.medical_id, 
+  p.first_name AS prisoner_name, 
+  s.first_name AS doctor_name, 
+  mr.diagnosis, mr.treatment, 
+  mr.date_of_examination
+FROM MedicalRecord mr
+JOIN Prisoner p ON mr.prisoner_id = p.prisoner_id
+JOIN Staff s ON mr.doctor_id = s.staff_id;
