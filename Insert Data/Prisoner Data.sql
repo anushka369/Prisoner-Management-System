b35b2316a20ad550c96519e9b933334bb5043b18
anushka <81908360+anushka369@users.noreply.github.com>
2025-04-07 22:37:28 +0530

    Update Prisoner Data.sql

--- Insert Data/Prisoner Data.sql
+++ Insert Data/Prisoner Data.sql
@@ -1,6 +1,6 @@
 -- Insert sample data into Prisoner
 INSERT INTO Prisoner (first_name, last_name, gender, date_of_birth, date_of_incarceration, crime_committed, status, cell_id)
 VALUES 
-('John', 'Doe', 'Male', '1985-05-15', '2020-01-01', 'Robbery', 'Incarcerated', 1),
-('Jane', 'Smith', 'Female', '1990-08-25', '2019-03-15', 'Fraud', 'Incarcerated', 2),
-('Mike', 'Johnson', 'Male', '1978-12-10', '2021-07-20', 'Assault', 'Paroled', 3);
+('Anushka', 'Banerjee', 'Female', '2004-10-15', '2020-01-01', 'Robbery', 'Paroled', 1),
+('Shashank', 'Singh', 'Male', '2005-07-21', '2019-03-15', 'Fraud', 'Incarcerated', 2),
+('Anurag', 'Ghosh', 'Male', '2005-08-13', '2021-07-20', 'Assault', 'Incarcerated', 3);
