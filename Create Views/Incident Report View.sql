a7d00fadad01451d8c1a57e2881596447ee4ccbf
anushka <81908360+anushka369@users.noreply.github.com>
2025-04-07 22:14:39 +0530

    Update Incident Report View.sql

--- Create Views/Incident Report View.sql
+++ Create Views/Incident Report View.sql
@@ -2,7 +2,12 @@ USE PRISONER_MANAGEMENT;
 
 -- View for Incident Report
 CREATE VIEW IncidentReportsView AS
-SELECT ir.report_id, p.first_name AS prisoner_name, s.first_name AS staff_name, ir.incident_description, ir.incident_date
+SELECT 
+  ir.report_id, 
+  p.first_name AS prisoner_name, 
+  s.first_name AS staff_name, 
+  ir.incident_description, 
+  ir.incident_date
 FROM IncidentReport ir
 JOIN Prisoner p ON ir.prisoner_id = p.prisoner_id
 JOIN Staff s ON ir.staff_id = s.staff_id;
