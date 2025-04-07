bc845bd57e61a48854658e906440cca2f02757c2
anushka <81908360+anushka369@users.noreply.github.com>
2025-04-07 22:15:35 +0530

    Update Rehabilitation Program View.sql

--- Create Views/Rehabilitation Program View.sql
+++ Create Views/Rehabilitation Program View.sql
@@ -2,6 +2,12 @@ USE PRISONER_MANAGEMENT;
 
 -- View for Rehabilitation Program
 CREATE VIEW RehabilitationProgramsView AS
-SELECT rp.program_id, p.first_name AS prisoner_name, rp.program_name, rp.start_date, rp.end_date, rp.status
+SELECT 
+  rp.program_id, 
+  p.first_name AS prisoner_name, 
+  rp.program_name, 
+  rp.start_date, 
+  rp.end_date, 
+  rp.status
 FROM RehabilitationProgram rp
 JOIN Prisoner p ON rp.prisoner_id = p.prisoner_id;
